d53d98075aeb8b349bd380a1c80a1cc5e4290d37
Burak Tüysüz <165570682+BurakTuysuz@users.noreply.github.com>
2024-06-23 20:33:00 +0300

    Add files via upload

--- mebspammer.py
+++ mebspammer.py
@@ -9,24 +9,33 @@ import requests
 import os
 
 
-def drivermain(link):
-    driver = webdriver.Chrome()
-    driver.get(link)
-    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='begen' and "
-                                                                                   "@onclick]")))
-    return driver, button
+def calistiralacaktarayici(browsername):
+    low = browsername.lower()
+
+    if low == 'chrome' or low == 'google' or low == 'googlechrome' or low == 'google chrome':
+        return webdriver.Chrome()
+    elif low == 'firefox':
+        return webdriver.Firefox()
+    elif low == 'edge':
+        return webdriver.Edge()
+    else:
+        print("\n", "Lütfen yalnızca Google chrome, Firefox veya Edge Tarayıcısı'nın ismini girin")
 
 
-def a(url):
+def a(url, tarayici):
     try:
         while True:
-            driver, button = drivermain(url)
+            driver = calistiralacaktarayici(tarayici)
+            driver.get(url)
+            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='begen' and "
+                                                                                           "@onclick]")))
             button.click()
             driver.quit()
-            drivermain(url)
 
     except TimeoutException:
-        print("Hata: Düğme bulunamadı veya zaman aşımı oldu")
+        print("\n", "Hata: Düğme bulunamadı veya zaman aşımı oldu")
+    except Exception as e:
+        print("\n", f"Hata Buton Bulunamadı: {str(e)}")
 
 
 def isim():
@@ -50,24 +59,31 @@ def isim():
 
 
 def anakod():
-    print('\n')
-    sor = input("Okul Sitesindeki İçerik Linkini Yapıştırın: ")
-    try:
-        al = requests.get(sor)
-        if al.status_code == 200:
-            sayi = int(
-                input("İşlem Aynı Anda Kaç Defa Yapılsın (Yüksek Sayı Bilgisayarın Donmasına Neden Olabilir): "))
-            threads = [t.Thread(target=a, args=(sor,)) for _ in range(sayi)]
+    if os.name == "posix":
+        print('\n')
+        tarayicial = input('Bilgisayarınızda Yüklü Olan Web Tarayıcınızın İsmini Girin (Google, Firefox, Edge): ')
+        sor = input("Okul Sitesindeki İçerik Linkini Yapıştırın: ")
+        try:
+            al = requests.get(sor)
+            if al.status_code == 200:
+                sayi = int(
+                    input("İşlem Aynı Anda Kaç Defa Yapılsın (Yüksek Sayı Bilgisayarın Donmasına Neden Olabilir, "
+                          "Önerilen sayı: 5), (Açık olan programların kapatılması önerilir): "))
+                threads = [t.Thread(target=a, args=(sor, tarayicial)) for _ in range(sayi)]
 
-            for thread in threads:
-                thread.start()
-        else:
-            print("Geçersiz Site")
+                for thread in threads:
+                    thread.start()
+            else:
+                print("Geçersiz Site")
+
+        except requests.exceptions.RequestException as e:
+            print("\n", "Geçersiz Site: ", e)
             anakod()
+        except Exception as ee:
+            print("Hata: ", ee)
 
-    except requests.exceptions.RequestException as e:
-        print("Geçersiz Site: ", e)
-        anakod()
+    else:
+        print("İşletim Sisteminiz Yalnızca Debian Tabanlı Linux Dağıtımı Olmalı")
 
 
 if __name__ == '__main__':
